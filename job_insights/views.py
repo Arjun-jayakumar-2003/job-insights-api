523e9c1add65bda438dc9f74e34bda53b661841b
Arjun Jayakumar <arjunjayakumar34@gmail.com>
2026-04-20 17:43:44 +0530

    Add Adzuna API integration with role validation and return filtered job results

--- job_insights/views.py
+++ job_insights/views.py
@@ -1,15 +1,29 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+import requests
+import os
 
 
 @api_view(['GET'])
 def test_api(request):
-    role = request.GET.get('role')
-    location = request.GET.get('location')
+    url = "https://api.adzuna.com/v1/api/jobs/in/search/1"
+    role = request.GET.get("role")
+
     if not role:
         return Response({"error": "role is required"}, status=400)
+
+    params = {
+        "app_id": os.getenv("ADZUNA_APP_ID"),
+        "app_key": os.getenv("ADZUNA_APP_KEY"),
+        "what": role,
+        "results_per_page": 5
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code != 200:
+        return Response({"error": "Failed to fetch data"}, status=500)
     
-    return Response({
-        "role": role,
-        "location": location
-    })
+    data = response.json()
+    
+    return Response(data.get("results", []))
