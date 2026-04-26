aa328abd4026771992ef7d6c5ba2b05aa99374cc
Arjun Jayakumar <arjunjayakumar34@gmail.com>
2026-04-27 01:57:21 +0530

    Add JobQuery and JobInsight models with migrations and admin registration

--- job_insights/admin.py
+++ job_insights/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import JobQuery, JobInsight
 
-# Register your models here.
+admin.site.register(JobQuery)
+admin.site.register(JobInsight)
--- job_insights/models.py
+++ job_insights/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class JobQuery(models.Model):
+    role = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class JobInsight(models.Model):
+    query = models.ForeignKey(JobQuery, on_delete=models.CASCADE)
+    total_jobs = models.IntegerField()
+    top_skills = models.JSONField()
+    top_locations = models.JSONField()
+    average_salary = models.FloatField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
